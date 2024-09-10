614d6588d532660c470608140e6503074d4008d3
shuyuvankerkwijk <shuyuwuvk@gmail.com>
2024-09-09 22:39:39 -0700

    Added documentation to Sep7 UDP code

--- UDPsocket_Sep7_Windows.py
+++ UDPsocket_Sep7_Windows.py
@@ -16,11 +16,11 @@ filename = filenames[0]
 filename_idx = 0
 packet_idx = 0
 
-# Configuration
-UDP_IP = "10.20.3.2"
-UDP_PORT = 8
-LISTEN_IP = "10.20.1.3"
-LISTEN_PORT = 55151
+# Configuration of IP addresses
+UDP_IP = "10.20.3.2" # Micrcontroller IP
+UDP_PORT = 8 # Microcontroller port
+LISTEN_IP = "10.20.1.3" # Hut PC IP
+LISTEN_PORT = 55151 # Hut PC port
 PACKET_SIZE = 601*2 + 42  # 600 bytes of data + 42 bytes UDP header
 
 protocol = socket.SOCK_DGRAM  # SOCK_DGRAM is for UDP
@@ -30,42 +30,41 @@ ip_family = socket.AF_INET  # AF_INET is for ipv4
 sock = socket.socket(ip_family, protocol)
 
 # Bind to the specific IP and port
-sock.bind(("0.0.0.0", LISTEN_PORT))
+sock.bind((LISTEN_IP, LISTEN_PORT)) # Can try using sock.bind(("0.0.0.0", LISTEN_PORT)) instead if having problems
 
 print(f"Listening for UDP packets from {UDP_IP}:{UDP_PORT} on port {LISTEN_PORT}...")
 
+
+
+
+
+# PROCESS FUNCTION
 def process_payload(payload):
 
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-    current_time_ns = time.time_ns() % 1_000_000_000
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f") # Current datetime accurate to 1/10th of second
+    current_time_ns = time.time_ns() % 1_000_000_000 # Time in nanoseconds within the current second
 	
     payload_hex = payload.hex()
     
-    # Split payload_hex by the separator 'f0f0'
+    # Split payload_hex (a string) by the separator 'f0f0'
     samples_hex = payload_hex.split("89abcdef")
 	
     samples_int = []
     times_int = []
 	
     for i, sample_hex in enumerate(samples_hex):
-        if len(sample_hex) < 16:
-            if i != 100:
-                print(f"Warning: Skipping invalid sample at index {i}: {sample_hex} with surrounding {samples_hex[i-5:i+5]} and payload {payload_hex[i*16+i*4-50:i+i*16*4+50]}")
-            continue
-
-        # Convert each hex segment to an integer
         try:
-            # Adjust the slicing based on your actual format requirements
             sample_int = int(sample_hex[2:4] + sample_hex[0:2] + sample_hex[5:6], 16)
             time_int = int(sample_hex[14:16] + sample_hex[12:14] + sample_hex[10:12] + sample_hex[8:10], 16)
             samples_int.append(sample_int)
             times_int.append(time_int)
-            #print(f"Sample Int: {sample_int}, Time Int: {time_int}")
+            
         except ValueError as e:
             print(f"Error processing sample: {sample_hex} - {e}")
 	
+    # Add everything to the samples_int list which then gets written to a row of the csv (samples_int is chosen for convenience, could also make a new list and add everything to it)
     samples_int.extend(times_int)
-    samples_int.append(int( (samples_hex[-1][2:4] + samples_hex[-1][0:2]) ,16))
+    samples_int.append(int( (samples_hex[-1][2:4] + samples_hex[-1][0:2]) ,16)) # Packet number (last item in samples_hex)
     samples_int.append(current_time)
     samples_int.append(current_time_ns)
 
@@ -76,12 +75,17 @@ def process_payload(payload):
     
     print(len(samples_int), "samples written to file. First value:", samples_int[0])
 
+
+
+
+
+# MAIN LOOP
 try:
     while True:
         data, addr = sock.recvfrom(PACKET_SIZE)  # Receive packet 
         if addr[0] == UDP_IP and addr[1] == UDP_PORT:
-            data_payload = data[0:]  # Header is automatically removed
-            process_payload(data_payload)
+            data_payload = data[0:]  # UDP header is removed
+            process_payload(data_payload) # Process the payload (function above)
             packet_idx += 1
             if packet_idx == NUM_PACKETS_PER_FILE:
                 print("All packets processed. Next file initialized.")
